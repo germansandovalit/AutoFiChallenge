66b3c70be802b0b570ffd2903c4ef9995f84b70c
gergsandoval <germansandoval.it@gmail.com>
2023-07-07 19:08:16 -0300

    add stubbed responses as fixtures

--- cypress/e2e/login/steps/create_account.cy.ts
+++ cypress/e2e/login/steps/create_account.cy.ts
@@ -19,11 +19,16 @@ When("I create an account", () => {
   cy.get("#textSRegEmail").type(email)
   cy.get("#user_password").type(password)
 
-  cy.intercept(`${baseUrl}/market/sendMobileVerificationOtp.php`, { statusCode: 200, body: { status: 200, message: "OTP sent to the given Mobile Number .  Please use OTP to verify Mobile Number" }, delay: 2000 })
+  cy.fixture('sendOtpResponse.json').then(sendOtpResponse => {
+    cy.intercept(`${baseUrl}/market/sendMobileVerificationOtp.php`, sendOtpResponse)
+  })
+  
   cy.get("#validate_mobile_number").click()
   cy.get("#txtValidateMobileOTP").should('be.visible').type(otp)
 
-  cy.intercept(`${baseUrl}/market/validateEmailPhoneOTP.php`, { statusCode: 200, body: { flag: 0, message: 'OTP validation is successful' }, delay: 2000 })
+  cy.fixture('validateOtpResponse.json').then(validateOtpResponse => {
+    cy.intercept(`${baseUrl}/market/validateEmailPhoneOTP.php`, validateOtpResponse)
+  })
   cy.get("#validateMobileOtp").click()
 
   cy.intercept(`${baseUrl}/market/register.php`, { statusCode: 200, body: { url: '/index.htm' } })
